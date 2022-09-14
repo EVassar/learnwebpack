e315cab5562fa3cdc052512c5722878ac07b5d62
EVassar <erlend.vassar@tptlive.ee>
2022-09-14 13:20:17 +0300

    add twig support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const glob = require('glob');
+
+let htmlPlugins = [];
+
+let files = glob.sync('./src/views/*.twig');
+files.forEach(file => {
+  let htmlPlugin = new HtmlWebpackPlugin({
+    filename: file.split('/').pop().replace('.twig', '.html'),
+    template: file
+  });
+  htmlPlugins.push(htmlPlugin);
+});
+
 
 module.exports = {
   entry: './src/index.js',
@@ -17,21 +30,32 @@ module.exports = {
   },
   module: {
     rules: [
-      {
+        {
+        test: /\.twig$/,
+        use: {
+            loader: 'twig-loader',
+            options: {
+                // See options section below
+            },
+        },
+    },  
+    {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
+    },
+    {
         test: /\.scss$/i,
         use: [MiniCssExtractPlugin.loader,"css-loader", "sass-loader"],
-      },
+    },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-            title: 'Very cool',
-            template: "./src/index.html"
-        }),
+        ...htmlPlugins,
         new MiniCssExtractPlugin(),
     ],
 };
+
+
+node: {
+    fs: "empty" // avoids error messages
+}
